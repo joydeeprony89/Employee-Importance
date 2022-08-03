134476430fa14e75f7161f367e251d6d38164f40
jd_786 <jaydeep_shil@certree.com>
2022-08-03 17:35:46 +0530

    Add project files.

---
+++ Employee Importance/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Employee_Importance
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+
+
+  // Definition for Employee.
+  public class Employee
+  {
+    public int id;
+    public int importance;
+    public IList<int> subordinates;
+  }
+
+
+  class Solution
+  {
+    public int GetImportance(IList<Employee> employees, int id)
+    {
+      Dictionary<int, Employee> adj = new Dictionary<int, Employee>();
+      HashSet<int> visited = new HashSet<int>();
+      foreach (var employee in employees)
+      {
+        if (!adj.ContainsKey(employee.id))
+        {
+          adj[employee.id] = employee;
+        }
+      }
+
+      int total = 0;
+      void Dfs(int id)
+      {
+        var employee = adj[id];
+        total += employee.importance;
+        visited.Add(id);
+        foreach (var emp in employee.subordinates)
+        {
+          if (!visited.Contains(emp))
+          {
+            Dfs(emp);
+          }
+        }
+      }
+      Dfs(id);
+      return total;
+    }
+  }
+}
